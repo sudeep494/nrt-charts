1b40d3e66dc7b70bb3107e83d072a4bd280db012
sudeep shrestha <sudeep494@gmail.com>
2017-04-09 00:27:36 -0500

    misc commit

--- Example/app.js
+++ Example/app.js
@@ -5,7 +5,10 @@ var myapp = angular.module('exampleApp', ['nrt-charts']);
 myapp.controller('mainCtrl', function($scope) {
 
 	//solid gauge
-	$scope.solidGaugeData = {value:5, min:0, max:5};
+	$scope.schoolRating = {value:5, min:0, max:5};
+
+	//line
+	$scope.homeValueHistory = {value:5, min:0, max:5};
 
 	//pie chart
 
